1c5933587758e6e629a469e16d0909eaba68bfce
S-Maciejewski <sebastian_maciejewski@yahoo.com>
2018-12-18 09:26:05 +0100

    Poprawki spektrogramu

--- inf132275_inf132332.py
+++ inf132275_inf132332.py
@@ -27,13 +27,14 @@ print(sys.argv[1])  # sciezka do pliku
 samplingFreq, signal = wavfile.read(sys.argv[1])
 print(samplingFreq)
 
-signal = [s[0] for s in signal]
+# signal = [s[0] for s in signal] # gdy są dwa kanały
+# weź 
 fftNumbers, freqs, amps, shifts = getSignalParameters(
-    signal[::10], samplingFreq)
+    signal[int(len(signal)/2):int(len(signal)/2+len(signal)/4):1], samplingFreq)
 
 # Rysowanie spektrogramu
 fig = plt.figure(figsize=(15, 15), dpi=80)
 ax = fig.add_subplot(111)
-plt.yscale('log')
+# plt.yscale('log')
 ax.stem(freqs, amps, '-')
 plt.show()
