73bb0c27e66a972c420765fa97ae2b00c62db047
Jan Techner <jantechner@live.com>
2018-12-22 23:12:55 +0100

    Add check script

---
+++ check.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+result=0
+for file in `ls trainall/`; do
+    retVal=`python3 inf132275_inf132332.py trainall/$file`
+    correctGender=`echo $file | sed 's/.*_\(.*\).wav/\1/'`
+    if [ $correctGender = $retVal ]; then
+        echo 'Correct'
+        ((result++))
+    else 
+        echo $file 'INCORRECT'
+    fi
+done
+echo $((result*100/91))
